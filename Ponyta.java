670d52412cebcf79123ee656be0163e3d8021016
Bruno Torres <brunildo_9999@hotmail.com>
2016-10-14 01:12:36 -0500

    Clases de Monstruo Especifico del tipo fuego

---
+++ Ponyta.java
@@ -0,0 +1,47 @@
+/**
+ *Subclase Ponyta que es un monstruo especifico de tipo Electrico
+ *@author Bruno Torres
+ */
+public class Ponyta extends Fuego{
+	private final String nombreMonstruo="Ponyta";
+	private final int hpBase=23;
+	private final int ataqueBase=18;
+	private final int defensaBase=16;
+	private final int velocidadBase=20;
+	public final String ataqueClase="Infierno";
+	
+	/**
+	 *Constructor de la clase Ponyta
+	 *@param nombre String
+	 *@param nivel int
+	 */
+	public Ponyta(String nombre, int nivel){
+		
+		this.nivel=nivel;
+		this.estado="ok";
+		this.ataque=nivel*ataqueBase;
+		this.defensa=nivel*defensaBase;
+		this.velocidad=nivel*velocidadBase;
+		this.hp=nivel*hpBase;
+
+		if(nombre !=null)
+			this.apodo=nombre;
+		else
+			this.apodo=nombreMonstruo;
+	}
+
+	/**
+	 *Método que calcula e inflinge daño a un enemigo.
+	 *@param enemigo Monstruo
+	 */
+	@Override
+	public void ataque2(Monstruo enemigo){
+		float damage;
+		
+		if ((damage = (this.ataque - enemigo.defensa)*1.8f*multiplicadorElemental(enemigo)) <= 0)
+			damage = 0;
+
+		enemigo.hp -= damage;
+		System.out.println(this.apodo+" uso infierno. ¡"+enemigo.apodo+" pierde "+damage+" puntos de vida!");
+	}
+}
